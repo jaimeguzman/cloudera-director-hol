efb18293e720725e5128afa4f760c02c42be6997
unknown <srekapalli@sysgain.com>
2017-11-23 10:26:06 +0530

    update

--- scripts/hdfsdatalakesetup.sh
+++ scripts/hdfsdatalakesetup.sh
@@ -42,9 +42,9 @@ echo "Cloudera Hue Web UI Username/Password: admin/admin" >> /home/cloudera/Node
 echo "Your Datalake Directory for the testdrive: $datalakeDirName" >> /home/cloudera/NodeDetails
 echo "Your Datalake Endpoint for the testdrive: $datalakeEndPoint" >> /home/cloudera/NodeDetails
 echo "Your Datalakename: $datalakename" >> /home/cloudera/NodeDetails
-echo "Your ClientID: $clientid" >> /home/cloudera/NodeDetails
-echo "Your Clientsecret: $clientsecret" >> /home/cloudera/NodeDetails
-echo "Your TenantID: $tenantid" >> /home/cloudera/NodeDetails
+echo "Your ClientID: $clientId" >> /home/cloudera/NodeDetails
+echo "Your Clientsecret: $clientSecret" >> /home/cloudera/NodeDetails
+echo "Your TenantID: $tenantId" >> /home/cloudera/NodeDetails
 echo "Your SubscriptionID: $subscriptionid" >> /home/cloudera/NodeDetails
 echo "Your Output Data files on Datalake for the testdrive:  $datalakeEndPoint/$datalakeDirName/WordCount" >> /home/cloudera/NodeDetails
 #curl -X POST -u admin:admin 'http://'${MangerIP}':7180/api/v1/clusters/'${ClusterName}'/services/'${HDFS}'/commands/restart'
